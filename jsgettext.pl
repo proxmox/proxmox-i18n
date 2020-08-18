04b8b6c61a58d3620e216ebbe43faa321569a13f
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-08-18 18:28:35 +0200

    run make update
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- jsgettext.pl
+++ jsgettext.pl
@@ -55,12 +55,12 @@ PVE::Tools::run_command($findcmd, outfunc => sub {
 
 my $header = <<__EOD;
 Proxmox message catalog.
-Copyright (C) 2011-2017 Proxmox Server Solutions GmbH
+Copyright (C) 2011-2020 Proxmox Server Solutions GmbH
 This file is free software: you can redistribute it and\/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-Proxmox Support Team <support\@proxmox.com>, 2017.
+Proxmox Support Team <support\@proxmox.com>, 2020.
 __EOD
 
 my $ctime = scalar localtime;
