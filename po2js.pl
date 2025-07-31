da713ca43387bacd23b501ce3f6b15270efd2c05
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-07-31 13:45:37 +0200

    po2js: avoid tabs in generated JS code
    The diff is empty when viewed with ignore-whitespace option.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- po2js.pl
+++ po2js.pl
@@ -120,9 +120,9 @@ function fnv31a(text) {
     var len = text.length;
     var hval = 0x811c9dc5;
     for (var i = 0; i < len; i++) {
-	var c = text.charCodeAt(i);
-	hval ^= c;
-	hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
+        var c = text.charCodeAt(i);
+        hval ^= c;
+        hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
     }
     hval &= 0x7fffffff;
     return hval;
@@ -132,7 +132,7 @@ function gettext(buf) {
     var digest = fnv31a(buf);
     var data = __proxmox_i18n_msgcat__[digest];
     if (!data) {
-	return buf;
+        return buf;
     }
     return data[0] || buf;
 }
@@ -143,9 +143,9 @@ function ngettext(singular, plural, n) {
     const translation = __proxmox_i18n_plurals_msgcat__[digest];
     if (!translation || msg_idx >= translation.length) {
         if (n === 1) {
-	    return singular;
+            return singular;
         } else {
-	    return plural;
+            return plural;
         }
     }
     return translation[msg_idx];
